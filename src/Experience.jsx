e4c2509eaccfe03fb9e451173c34a8490dcec829
Azariah John <azariah1200@gmail.com>
2023-03-11 14:53:13 +0530

    run-a

---
+++ src/Experience.jsx
@@ -0,0 +1,42 @@
+import { useThree, extend } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Interactive } from '@react-three/xr'
+import { useState } from 'react'
+
+extend({ OrbitControls })
+
+export default function Experience()
+{
+    const { camera, gl } = useThree()
+    const [color, setColor] = useState("blue");
+
+    return <>
+
+        <orbitControls args={ [ camera, gl.domElement ] } />
+
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <Interactive onSelect={(e) => {
+            setColor("green")
+        }}>
+            <mesh position-x={ - 2 } onClick={(e) => {
+                setColor("green")
+            }}>
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
+        </Interactive>
+
+        <mesh position-x={ 2 } scale={ 1.5 } >
+            <boxGeometry />
+            <meshStandardMaterial color={color} />
+        </mesh>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
+
+    </>
+}
---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+import { VRButton, XR, Controllers, Hands } from '@react-three/xr'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <>
+    <VRButton/>
+        <Canvas
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [ 0, 1.6, 10 ],
+            } }
+            className="canvas"
+        >
+            <XR>
+                <Controllers />
+                <Experience />
+            </XR>
+        </Canvas>
+    </>
+)
