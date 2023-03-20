c32737fa5cd3d830ec8459cabef68e3cb3033ae2
Azariah John <azariah1200@gmail.com>
2023-03-20 22:45:55 +0530

    with interactive and without controller

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,6 +2,7 @@ import { useThree, extend } from '@react-three/fiber'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Interactive } from '@react-three/xr'
 import { useState } from 'react'
+import {Text} from '@react-three/drei'
 
 extend({ OrbitControls })
 
@@ -17,16 +18,18 @@ export default function Experience()
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        {/* <Interactive onSelect={(e) => {
+        <Interactive onSelect={(e) => {
             setColor("green")
-        }}> */}
+        }}>
             <mesh position-x={ - 2 } onClick={(e) => {
                 setColor("green")
             }}>
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
-        {/* </Interactive> */}
+        </Interactive>
+
+        <Text position={[0,1.5,0]} scale={0.3} color="black">With Interactive and without Controller</Text>
 
         <mesh position-x={ 2 } scale={ 1.5 } >
             <boxGeometry />
